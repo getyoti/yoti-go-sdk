5f3ede2c1a346753690e93d198d91151c4d12f3d
Edward Harrod <echarrod@users.noreply.github.com>
2018-03-15 16:33:48 +0000

    [SDK-251] Added AML Example

---
+++ amlexample/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/getyoti/yoti-go-sdk"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var sdkID = os.Getenv("YOTI_CLIENT_SDK_ID")
+var key, _ = ioutil.ReadFile(os.Getenv("YOTI_KEY_FILE_PATH"))
+var client = yoti.YotiClient{
+	SdkID: sdkID,
+	Key:   key}
+
+func main() {
+	givenNames := "Edward Richard George"
+	familyName := "Heath"
+
+	amlAddress := yoti.AmlAddress{
+		Country: "GBR"}
+
+	amlProfile := yoti.AmlProfile{
+		GivenNames: givenNames,
+		FamilyName: familyName,
+		Address:    amlAddress}
+
+	result, err := client.PerformAmlCheck(amlProfile)
+
+	if err != nil {
+		log.Printf(
+			fmt.Sprintf(
+				"Unable to retrieve AML result. Error: %s", err))
+	} else {
+		log.Printf(
+			fmt.Sprintf(
+				"AML Result for %s %s:",
+				givenNames,
+				familyName))
+		log.Printf(
+			fmt.Sprintf(
+				"On PEP list: %s",
+				strconv.FormatBool(result.OnPEPList)))
+		log.Printf(
+			fmt.Sprintf(
+				"On Fraud list: %s",
+				strconv.FormatBool(result.OnFraudList)))
+		log.Printf(
+			fmt.Sprintf(
+				"On Watch list: %s",
+				strconv.FormatBool(result.OnWatchList)))
+	}
+}
