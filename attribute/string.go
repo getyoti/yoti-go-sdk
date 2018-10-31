cf400b063611c9f7d9c612c2c1ea7bdd48becdfe
echarrod <echarrod@users.noreply.github.com>
2018-10-31 15:50:08 +0000

    [SDK-448]: Update attributes to embed `attribute` struct instead of redefining common fields

--- attribute/string.go
+++ attribute/string.go
@@ -1,23 +1,19 @@
 package attribute
 
-import (
-	"github.com/getyoti/yoti-go-sdk/anchor"
-)
-
 //String is a Yoti attribute which returns a string as its value
 type String struct {
-	Name    string
-	Value   string
-	Type    AttrType
-	Anchors []*anchor.Anchor
+	Attribute
+	Value string
 }
 
 //NewString creates a new String attribute
 func NewString(a *Attribute) *String {
 	return &String{
-		Name:    a.Name,
-		Value:   string(a.Value),
-		Type:    AttrTypeString,
-		Anchors: a.Anchors,
+		Attribute: Attribute{
+			Name:    a.Name,
+			Type:    AttrTypeString,
+			Anchors: a.Anchors,
+		},
+		Value: string(a.Value),
 	}
 }
