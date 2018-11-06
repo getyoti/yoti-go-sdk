77f982577a449237a7f1a6ca789d7a7da500bc23
echarrod <echarrod@users.noreply.github.com>
2018-11-06 16:12:48 +0000

    [SDK-597]: Update attribute to use new image object

--- image.go
+++ image.go
@@ -16,13 +16,14 @@ const (
 	ImageTypeOther
 )
 
+//Deprecated: Will be removed in v3.0.0 - use attribute.Image instead. ImageType Image format
 //Image format of the image and the image data
 type Image struct {
 	Type ImageType
 	Data []byte
 }
 
-// Deprecated: Will be removed in v3.0.0, please use GetMimeType instead. GetContentType returns the MIME type of this piece of Yoti user information. For more information see:
+// Deprecated: Will be removed in v3.0.0, please use attribute.GetMIMEType instead. GetContentType returns the MIME type of this piece of Yoti user information. For more information see:
 // https://en.wikipedia.org/wiki/Media_type
 func (image *Image) GetContentType() string {
 	switch image.Type {
@@ -37,7 +38,7 @@ func (image *Image) GetContentType() string {
 	}
 }
 
-//Deprecated: Will be removed in v3.0.0, please use Base64URL() instead. URL Image encoded in a base64 URL
+//Deprecated: Will be removed in v3.0.0, please use ImageAttribute.Base64URL() instead. URL Image encoded in a base64 URL
 func (image *Image) URL() string {
 	base64EncodedImage := base64.StdEncoding.EncodeToString(image.Data)
 	return "data:" + image.GetContentType() + ";base64;," + base64EncodedImage
