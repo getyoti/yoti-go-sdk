f76cf8861d1db80d2956cfd88ef21c0d1dfb7508
echarrod <echarrod@users.noreply.github.com>
2018-11-06 17:34:22 +0000

    [SDK-597]: Remove mime.go: move GetMIMEType and Base64URL to attribute.image

--- attribute/image.go
+++ attribute/image.go
@@ -1,5 +1,10 @@
 package attribute
 
+import (
+	"encoding/base64"
+	"log"
+)
+
 //ImageType Image format
 type ImageType int
 
@@ -16,4 +21,28 @@ const (
 type Image struct {
 	Type ImageType
 	Data []byte
+}
+
+var mimeTypeMap = map[ImageType]string{
+	ImageTypeJpeg: "image/jpeg",
+	ImageTypePng:  "image/png",
+}
+
+// GetMIMEType returns the MIME type of this piece of Yoti user information. For more information see:
+// https://en.wikipedia.org/wiki/Media_type
+func GetMIMEType(imageType ImageType) (result string) {
+	if val, ok := mimeTypeMap[imageType]; ok {
+		return val
+	}
+
+	log.Printf("Unable to find a matching MIME type for value type %q", imageType)
+	return
+}
+
+// Base64URL is the Image encoded as a base64 URL
+func (image *Image) Base64URL() (string, error) {
+	base64EncodedImage := base64.StdEncoding.EncodeToString(image.Data)
+	contentType := GetMIMEType(image.Type)
+
+	return "data:" + contentType + ";base64;," + base64EncodedImage, nil
 }
--- attribute/imageattribute.go
+++ attribute/imageattribute.go
@@ -1,8 +1,6 @@
 package attribute
 
 import (
-	"encoding/base64"
-
 	"github.com/getyoti/yoti-go-sdk/anchor"
 	"github.com/getyoti/yoti-go-sdk/yotiprotoattr_v3"
 )
@@ -40,12 +38,4 @@ func NewImage(a *yotiprotoattr_v3.Attribute) *ImageAttribute {
 		},
 		Anchors: anchor.ParseAnchors(a.Anchors),
 	}
-}
-
-// Base64URL is the Image encoded as a base64 URL
-func (imageAttribute *ImageAttribute) Base64URL() (string, error) {
-	base64EncodedImage := base64.StdEncoding.EncodeToString(imageAttribute.Value.Data)
-	contentType := GetMIMEType(imageAttribute.ContentType)
-
-	return "data:" + contentType + ";base64;," + base64EncodedImage, nil
 }
