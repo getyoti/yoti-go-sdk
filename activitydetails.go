8b21de9f584b954ed98c88451ba9d2971092b1a4
echarrod <echarrod@users.noreply.github.com>
2019-03-12 10:17:30 +0000

    SDK-712: Add ParentRememberMeID

--- activitydetails.go
+++ activitydetails.go
@@ -2,13 +2,22 @@ package yoti
 
 // ActivityDetails represents the result of an activity between a user and the application.
 type ActivityDetails struct {
-	UserProfile  Profile
-	rememberMeID string
+	UserProfile        Profile
+	rememberMeID       string
+	parentRememberMeID string
 }
 
-// RememberMeID is a unique identifier Yoti assigns to your user, but only for your app.
-// If the same user logs into your app again, you get the same id.
-// If she/he logs into another application, Yoti will assign a different id for that app.
+// RememberMeID is a unique, stable identifier for a user in the context
+// of an application. You can use it to identify returning users.
+// This value will be different for the same user in different applications.
 func (a ActivityDetails) RememberMeID() string {
 	return a.rememberMeID
+}
+
+// ParentRememberMeID is a unique, stable identifier for a user in the
+// context of an organisation. You can use it to identify returning users.
+// This value is consistent for a given user across different applications
+// belonging to a single organisation.
+func (a ActivityDetails) ParentRememberMeID() string {
+	return a.parentRememberMeID
 }
