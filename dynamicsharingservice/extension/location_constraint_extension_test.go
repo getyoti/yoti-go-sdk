798df4d8696143b0fca8de5c76605b6a809c02ad
Emma Smith <Emma.Smith@Yoti.com>
2019-08-08 13:58:09 +0100

    SDK-723: Fix incorrect test expectation for LocationConstraintExtension example

--- dynamicsharingservice/extension/location_constraint_extension_test.go
+++ dynamicsharingservice/extension/location_constraint_extension_test.go
@@ -14,5 +14,5 @@ func ExampleLocationConstraintExtension() {
 
 	data, _ := extension.MarshalJSON()
 	fmt.Println(string(data))
-	// Output: {"latitude":51.511831,"longtitude":-0.081446,"radius":0.001,"max_uncertainty_radius":0.001}
+	// Output: {"type":"LOCATION_CONSTRAINT","content":{"latitude":51.511831,"longtitude":-0.081446,"radius":0.001,"max_uncertainty_radius":0.001}}
 }
