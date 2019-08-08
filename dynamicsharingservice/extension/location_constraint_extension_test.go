06ba31860c8f63703dc3933d95881b8fcaa1e8af
Emma Smith <Emma.Smith@Yoti.com>
2019-08-08 12:51:23 +0100

    SDK-723: Add LocationConstraintExtension

---
+++ dynamicsharingservice/extension/location_constraint_extension_test.go
@@ -0,0 +1,18 @@
+package extension
+
+import (
+	"fmt"
+)
+
+func ExampleLocationConstraintExtension() {
+	extension := (&LocationConstraintExtensionBuilder{}).New().
+		WithLatitude(51.511831).
+		WithLongtitude(-0.081446).
+		WithRadius(0.001).
+		WithUncertainty(0.001).
+		Build()
+
+	data, _ := extension.MarshalJSON()
+	fmt.Println(string(data))
+	// Output: {"latitude":51.511831,"longtitude":-0.081446,"radius":0.001,"max_uncertainty_radius":0.001}
+}
