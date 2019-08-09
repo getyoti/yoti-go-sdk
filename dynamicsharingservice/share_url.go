4bc4f06b1075a5b9e65e5e72c94d79176d387b9b
Emma Smith <Emma.Smith@Yoti.com>
2019-08-09 14:49:24 +0100

    SDK-723: Add ShareURL
    CreateShareURL can create a dynamic share QR code
    
    MakeRequest and GetDynamicShareEndpoint had to be added to the parent
    Yoti package so they'd be accessible by the child package

---
+++ dynamicsharingservice/share_url.go
@@ -0,0 +1,44 @@
+package dynamicsharingservice
+
+import (
+	"encoding/json"
+
+	yoti "github.com/getyoti/yoti-go-sdk/v2"
+)
+
+var (
+	// ShareURLHTTPErrorMessages specifies the HTTP error status codes used
+	// by the Share URL API
+	ShareURLHTTPErrorMessages = map[int]string{
+		400: "JSON is incorrect, contains invalid data: %[2]s",
+		404: "Application was not found: %[2]s",
+	}
+)
+
+// ShareURL contains a dynamic share qr code
+type ShareURL struct {
+	ShareURL string `json:"qrcode"`
+	RefID    string `json:"ref_id"`
+}
+
+// CreateShareURL creates a QR Code for a dynamic scenario
+func CreateShareURL(client *yoti.Client, scenario *DynamicScenario) (share ShareURL, err error) {
+	httpMethod := "POST"
+	endpoint, err := yoti.GetDynamicShareEndpoint(client)
+	if err != nil {
+		return
+	}
+	payload, err := scenario.MarshalJSON()
+	if err != nil {
+		return
+	}
+
+	response, err := client.MakeRequest(httpMethod, endpoint, payload, ShareURLHTTPErrorMessages, yoti.DefaultHTTPErrorMessages)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal([]byte(response), &share)
+
+	return
+}
