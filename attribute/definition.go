1b27e9181017e47c84f2e38e7f136635dff6933b
Alex Burt <alex.burt@yoti.com>
2019-10-30 16:28:46 +0000

    SDK-1261: Add support for third party extension

--- attribute/definition.go
+++ attribute/definition.go
@@ -1,5 +1,9 @@
 package attribute
 
+import (
+	"encoding/json"
+)
+
 // AttributeDefinition contains information about the attribute(s) issued by a third party.
 type AttributeDefinition struct {
 	name string
@@ -8,4 +12,20 @@ type AttributeDefinition struct {
 // Name of the attribute to be issued.
 func (a AttributeDefinition) Name() string {
 	return a.name
+}
+
+// MarshalJSON returns encoded json
+func (a AttributeDefinition) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Name string `json:"name"`
+	}{
+		Name: a.name,
+	})
+}
+
+// NewAttributeDefinition returns a new AttributeDefinition
+func NewAttributeDefinition(s string) AttributeDefinition {
+	return AttributeDefinition{
+		name: s,
+	}
 }
