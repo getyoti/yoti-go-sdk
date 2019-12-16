e1331d1751335ff6249a0b291a33a5eed6a40533
Emma Smith <52929061+emmas-yoti@users.noreply.github.com>
2019-12-16 16:22:17 +0000

    SDK-1275: Change ActivityDetails errstrings to provide something more meaningful (#74)
    * SDK-1275: Add TemporaryError to wrap errors that can be handled by
    retrying
    
    * SDK-1275: Return error type instead of []string to indicate errors
    
    * SDK-1275: Update example project to expect error instead of errStrings
    
    * SDK-1275: Add documentation describing how to use temporary errors
    
    * SDK-1275: Implement golang Temporary interface on TemporaryError
    
    * SDK-1275: Improve comment for GetActivityDetails errors
    
    * SDK-1275: Improve README for section Handling Errors
    
    * SDK-1275: Fix punctuation in documentation
    
    Co-Authored-By: Ed Harrod <echarrod@users.noreply.github.com>
    
    * SDK-1275: Reformat documentation for GetActivityDetails
    
    Reformat the documentation so the lines are of uniform length
    
    * SDK-1275: Change documentation to refer to Temporary interface
    
    Co-Authored-By: piersy <pierspowlesland@gmail.com>
    
    * SDK-1275: Assert error temporary-ness in unit tests
    
    * SDK-1275: Fix issue displaying error messages in example project
    
    * SDK-1275: Fix formatting error for travis and go fmt for go 1.9
    
    * SDK-1275: Update InvalidToken test to use HTTPClient
    
    * SDK-1275: Fix compilation failure in InvalidToken test

---
+++ errors.go
@@ -0,0 +1,21 @@
+package yoti
+
+// TemporaryError indicates that a temporary outage has occured and the
+// previous request can be reattempted without modification.
+type TemporaryError struct {
+	Err error
+}
+
+func (e TemporaryError) Error() string {
+	return e.Err.Error()
+}
+
+// Unwrap returns the internal error for debugging
+func (e TemporaryError) Unwrap() error {
+	return e.Err
+}
+
+// Temporary indicates this error is a temporary error
+func (e TemporaryError) Temporary() bool {
+	return true
+}
