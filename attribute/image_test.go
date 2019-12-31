94fe21fcac39ce6b90cc92ea53013742e79d77ef
Emma Smith <Emma.Smith@Yoti.com>
2019-12-31 14:39:35 +0000

    SDK-537: Move attribute tests into separate files in attribute subpackage

---
+++ attribute/image_test.go
@@ -0,0 +1,105 @@
+package attribute
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/getyoti/yoti-go-sdk/v2/consts"
+	"github.com/getyoti/yoti-go-sdk/v2/yotiprotoattr"
+	"gotest.tools/assert"
+)
+
+func TestImageAttribute_Image_Png(t *testing.T) {
+	attributeName := consts.AttrSelfie
+	byteValue := []byte("value")
+
+	var attributeImage = &yotiprotoattr.Attribute{
+		Name:        attributeName,
+		Value:       byteValue,
+		ContentType: yotiprotoattr.ContentType_PNG,
+		Anchors:     []*yotiprotoattr.Anchor{},
+	}
+
+	selfie, err := NewImage(attributeImage)
+	assert.NilError(t, err)
+
+	assert.DeepEqual(t, selfie.Value().Data, byteValue)
+}
+
+func TestImageAttribute_Image_Jpeg(t *testing.T) {
+	attributeName := consts.AttrSelfie
+	byteValue := []byte("value")
+
+	var attributeImage = &yotiprotoattr.Attribute{
+		Name:        attributeName,
+		Value:       byteValue,
+		ContentType: yotiprotoattr.ContentType_JPEG,
+		Anchors:     []*yotiprotoattr.Anchor{},
+	}
+
+	selfie, err := NewImage(attributeImage)
+	assert.NilError(t, err)
+
+	assert.DeepEqual(t, selfie.Value().Data, byteValue)
+}
+
+func TestImageAttribute_Image_Default(t *testing.T) {
+	attributeName := consts.AttrSelfie
+	byteValue := []byte("value")
+
+	var attributeImage = &yotiprotoattr.Attribute{
+		Name:        attributeName,
+		Value:       byteValue,
+		ContentType: yotiprotoattr.ContentType_PNG,
+		Anchors:     []*yotiprotoattr.Anchor{},
+	}
+	selfie, err := NewImage(attributeImage)
+	assert.NilError(t, err)
+
+	assert.DeepEqual(t, selfie.Value().Data, byteValue)
+}
+func TestImageAttribute_Base64Selfie_Png(t *testing.T) {
+	attributeName := consts.AttrSelfie
+	imageBytes := []byte("value")
+
+	var attributeImage = &yotiprotoattr.Attribute{
+		Name:        attributeName,
+		Value:       imageBytes,
+		ContentType: yotiprotoattr.ContentType_PNG,
+		Anchors:     []*yotiprotoattr.Anchor{},
+	}
+
+	selfie, err := NewImage(attributeImage)
+	assert.NilError(t, err)
+
+	base64ImageExpectedValue := base64.StdEncoding.EncodeToString(imageBytes)
+
+	expectedBase64Selfie := "data:image/png;base64;," + base64ImageExpectedValue
+
+	base64Selfie := selfie.Value().Base64URL()
+
+	assert.Equal(t, base64Selfie, expectedBase64Selfie)
+}
+
+func TestImageAttribute_Base64URL_Jpeg(t *testing.T) {
+	attributeName := consts.AttrSelfie
+	imageBytes := []byte("value")
+
+	var attributeImage = &yotiprotoattr.Attribute{
+		Name:        attributeName,
+		Value:       imageBytes,
+		ContentType: yotiprotoattr.ContentType_JPEG,
+		Anchors:     []*yotiprotoattr.Anchor{},
+	}
+
+	selfie, err := NewImage(attributeImage)
+	assert.NilError(t, err)
+
+	base64ImageExpectedValue := base64.StdEncoding.EncodeToString(imageBytes)
+
+	expectedBase64Selfie := "data:image/jpeg;base64;," + base64ImageExpectedValue
+
+	base64Selfie := selfie.Value().Base64URL()
+
+	assert.Equal(t, base64Selfie, expectedBase64Selfie)
+}
--- attribute/time_attribute_test.go
+++ attribute/time_attribute_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/getyoti/yoti-go-sdk/v2/yotiprotoattr"
 	"gotest.tools/assert"
+	is "gotest.tools/assert/cmp"
 )
 
 func TestTimeAttribute_NewTime_DateOnly(t *testing.T) {
@@ -17,4 +18,16 @@ func TestTimeAttribute_NewTime_DateOnly(t *testing.T) {
 	assert.NilError(t, err)
 
 	assert.Equal(t, *timeAttribute.Value(), time.Date(2011, 12, 25, 0, 0, 0, 0, time.UTC))
+}
+func TestTimeAttribute_DateOfBirth(t *testing.T) {
+	protoAttribute := createAttributeFromTestFile(t, "../test/fixtures/test_attribute_date_of_birth.txt")
+
+	dateOfBirthAttribute, err := NewTime(protoAttribute)
+
+	assert.Assert(t, is.Nil(err))
+
+	expectedDateOfBirth := time.Date(1970, time.December, 01, 0, 0, 0, 0, time.UTC)
+	actualDateOfBirth := dateOfBirthAttribute.Value()
+
+	assert.Assert(t, actualDateOfBirth.Equal(expectedDateOfBirth))
 }
