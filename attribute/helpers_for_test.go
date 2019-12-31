18b670841c04d3c774ddb3232ee4b5e593360cae
Emma Smith <Emma.Smith@Yoti.com>
2019-12-31 14:39:35 +0000

    SDK-537: Move attribute tests into separate files in attribute subpackage

---
+++ attribute/helpers_for_test.go
@@ -0,0 +1,23 @@
+package attribute
+
+import (
+	"testing"
+
+	"github.com/getyoti/yoti-go-sdk/v2/test"
+	"github.com/getyoti/yoti-go-sdk/v2/yotiprotoattr"
+	"github.com/golang/protobuf/proto"
+	"gotest.tools/assert"
+	is "gotest.tools/assert/cmp"
+)
+
+func createAttributeFromTestFile(t *testing.T, filename string) *yotiprotoattr.Attribute {
+	attributeBytes := test.DecodeTestFile(t, filename)
+
+	attributeStruct := &yotiprotoattr.Attribute{}
+
+	err2 := proto.Unmarshal(attributeBytes, attributeStruct)
+
+	assert.Assert(t, is.Nil(err2))
+
+	return attributeStruct
+}
