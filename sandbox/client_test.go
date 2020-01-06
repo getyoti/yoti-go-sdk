f1a7fd424b69890cd4bffb019fc0b2cbe835d634
Emma Smith <Emma.Smith@Yoti.com>
2020-01-06 16:51:43 +0000

    SDK-1343: Add HTTP request mocking to sandbox client to allow testing

---
+++ sandbox/client_test.go
@@ -0,0 +1,41 @@
+package sandbox
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+type mockHTTPClient struct {
+	do func(*http.Request) (*http.Response, error)
+}
+
+func (mock *mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	return mock.do(req)
+}
+
+func TestClient_SetupSharingProfile_Success(t *testing.T) {
+	expectedToken := "shareToken"
+	key, _ := rsa.GenerateKey(rand.Reader, 1024)
+	client := Client{
+		Key:     key,
+		BaseURL: "example.com",
+		HTTPClient: &mockHTTPClient{
+			do: func(*http.Request) (*http.Response, error) {
+				return &http.Response{
+					StatusCode: 201,
+					Body:       ioutil.NopCloser(strings.NewReader(`{"token":"` + expectedToken + `"}`)),
+				}, nil
+			},
+		},
+	}
+	token, err := client.SetupSharingProfile(Profile{})
+	assert.NilError(t, err)
+
+	assert.Equal(t, token, expectedToken)
+}
