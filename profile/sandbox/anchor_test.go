04efe9fa907f86b29a06159e46669ebc6ba5539e
Emma Smith <Emma.Smith@Yoti.com>
2020-01-08 15:25:12 +0000

    SDK-1343: Get sandbox coverage up to 90%

--- profile/sandbox/anchor_test.go
+++ profile/sandbox/anchor_test.go
@@ -1,6 +1,7 @@
 package sandbox
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
@@ -8,13 +9,15 @@ import (
 func ExampleSourceAnchor() {
 	time.Local = time.UTC
 	source := SourceAnchor("subtype", time.Unix(1234567890, 0), "value")
-	fmt.Println(source)
-	// Output: {SOURCE value subtype 2009-02-13 23:31:30 +0000 UTC}
+	marshalled, _ := json.Marshal(source)
+	fmt.Println(string(marshalled))
+	// Output: {"type":"SOURCE","value":"value","sub_type":"subtype","timestamp":1234567890000}
 }
 
 func ExampleVerifierAnchor() {
 	time.Local = time.UTC
 	verifier := VerifierAnchor("subtype", time.Unix(1234567890, 0), "value")
-	fmt.Println(verifier)
-	// Output: {VERIFIER value subtype 2009-02-13 23:31:30 +0000 UTC}
+	marshalled, _ := json.Marshal(verifier)
+	fmt.Println(string(marshalled))
+	// Output: {"type":"VERIFIER","value":"value","sub_type":"subtype","timestamp":1234567890000}
 }
--- sandbox/client_test.go
+++ sandbox/client_test.go
@@ -19,6 +19,24 @@ func (mock *mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	return mock.do(req)
 }
 
+func TestClient_SetupSharingProfile_ShouldReturnErrorIfProfileNotCreated(t *testing.T) {
+	key, _ := rsa.GenerateKey(rand.Reader, 1024)
+	client := Client{
+		Key:     key,
+		BaseURL: "example.com",
+		HTTPClient: &mockHTTPClient{
+			do: func(*http.Request) (*http.Response, error) {
+				return &http.Response{
+					StatusCode: 401,
+					Body:       ioutil.NopCloser(strings.NewReader("")),
+				}, nil
+			},
+		},
+	}
+	_, err := client.SetupSharingProfile(Profile{})
+	assert.ErrorContains(t, err, "Sharing Profile not created")
+}
+
 func TestClient_SetupSharingProfile_Success(t *testing.T) {
 	expectedToken := "shareToken"
 	key, _ := rsa.GenerateKey(rand.Reader, 1024)
