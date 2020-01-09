ba0128b51c99dba1424799663be05c249ba918f4
Emma Smith <Emma.Smith@Yoti.com>
2020-01-09 12:50:45 +0000

    SDK-1367: Unit tests for parsing expiry dates based on acceptance criteria

--- attribute/issuance_details_test.go
+++ attribute/issuance_details_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/getyoti/yoti-go-sdk/v2/test"
 	"github.com/getyoti/yoti-go-sdk/v2/yotiprotoshare"
@@ -46,6 +47,86 @@ func TestShouldLogWarningIfErrorInParsingExpiryDate(t *testing.T) {
 	assert.Equal(t, expectedBase64Token, result.Token())
 	assert.Assert(t, is.Nil(result.ExpiryDate()))
 	assert.Equal(t, "parsing time \"2006-13-02T15:04:05.000Z\": month out of range", err.Error())
+}
+
+func TestIssuanceDetails_parseExpiryDate_ShouldParseAllRFC3339Formats(t *testing.T) {
+	table := []struct {
+		Input    string
+		Expected time.Time
+	}{
+		{
+			Input:    "2006-01-02T22:04:05Z",
+			Expected: time.Date(2006, 01, 02, 22, 4, 5, 0, time.UTC),
+		},
+		{
+			Input:    "2010-05-20T10:44:25Z",
+			Expected: time.Date(2010, 5, 20, 10, 44, 25, 0, time.UTC),
+		},
+		{
+			Input:    "2006-01-02T22:04:05.1Z",
+			Expected: time.Date(2006, 1, 2, 22, 4, 5, 100e6, time.UTC),
+		},
+		{
+			Input:    "2012-03-06T04:20:07.5Z",
+			Expected: time.Date(2012, 3, 6, 4, 20, 7, 500e6, time.UTC),
+		},
+		{
+			Input:    "2006-01-02T22:04:05.12Z",
+			Expected: time.Date(2006, 1, 2, 22, 4, 5, 120e6, time.UTC),
+		},
+		{
+			Input:    "2013-03-04T20:43:55.56Z",
+			Expected: time.Date(2013, 3, 4, 20, 43, 55, 560e6, time.UTC),
+		},
+		{
+			Input:    "2006-01-02T22:04:05.123Z",
+			Expected: time.Date(2006, 1, 2, 22, 4, 5, 123e6, time.UTC),
+		},
+		{
+			Input:    "2007-04-07T17:34:11.784Z",
+			Expected: time.Date(2007, 4, 7, 17, 34, 11, 784e6, time.UTC),
+		},
+		{
+			Input:    "2006-01-02T22:04:05.1234Z",
+			Expected: time.Date(2006, 1, 2, 22, 4, 5, 123400e3, time.UTC),
+		},
+		{
+			Input:    "2017-09-14T16:54:30.4784Z",
+			Expected: time.Date(2017, 9, 14, 16, 54, 30, 478400e3, time.UTC),
+		},
+		{
+			Input:    "2006-01-02T22:04:05.12345Z",
+			Expected: time.Date(2006, 1, 2, 22, 4, 5, 123450e3, time.UTC),
+		},
+		{
+			Input:    "2009-06-07T14:20:30.74622Z",
+			Expected: time.Date(2009, 6, 7, 14, 20, 30, 746220e3, time.UTC),
+		},
+		{
+			Input:    "2006-01-02T22:04:05.123456Z",
+			Expected: time.Date(2006, 1, 2, 22, 4, 5, 123456e3, time.UTC),
+		},
+		{
+			Input:    "2008-10-25T06:50:55.643562Z",
+			Expected: time.Date(2008, 10, 25, 6, 50, 55, 643562e3, time.UTC),
+		},
+		{
+			Input:    "2002-10-02T10:00:00-05:00",
+			Expected: time.Date(2002, 10, 2, 10, 0, 0, 0, time.FixedZone("-0500", -5*60*60)),
+		},
+		{
+			Input:    "2002-10-02T10:00:00+11:00",
+			Expected: time.Date(2002, 10, 2, 10, 0, 0, 0, time.FixedZone("+1100", 11*60*60)),
+		},
+	}
+
+	for _, row := range table {
+		func(input string, expected time.Time) {
+			expiryDate, err := parseExpiryDate(input)
+			assert.NilError(t, err)
+			assert.Equal(t, expiryDate.UTC(), expected.UTC())
+		}(row.Input, row.Expected)
+	}
 }
 
 func TestInvalidProtobufThrowsError(t *testing.T) {
