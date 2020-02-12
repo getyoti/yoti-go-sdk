c2824eb27aad4274d9fbae5d478af162d2d70991
Emma Smith <Emma.Smith@Yoti.com>
2020-02-12 11:28:39 +0000

    Update gotest.tool dependency to v3 from v2
    Attempting to fix issue with Travis builds failing on pre-modules
    versions of Golang by making version used for gotest module explicit in
    import

--- share/extra_data_test.go
+++ share/extra_data_test.go
@@ -10,8 +10,8 @@ import (
 	"github.com/getyoti/yoti-go-sdk/v2/yotiprotoshare"
 	"github.com/golang/protobuf/proto"
 
-	"gotest.tools/assert"
-	is "gotest.tools/assert/cmp"
+	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
 )
 
 func TestAttributeIssuanceDetailsShouldReturnNilWhenNoDataEntries(t *testing.T) {
