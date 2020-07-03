26651afebd913fc6dea399f694d85ac247176d13
echarrod <echarrod@users.noreply.github.com>
2020-07-03 08:47:57 +0100

    SDk-1495: Rework pre-commit: ensure works on all OS
    Use `go-unit-tests` from pre-commit-golang instead of custom script. Use script for go-fmt to ensure cross-platform compatiblity. Ignore deprecated package warning (for Protobuf package, will remove in major)

--- gofmt.sh
+++ gofmt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 unset dirs files
 dirs=$(go list -f {{.Dir}} ./... | grep -v /yotiprotoshare/ | grep -v /yotiprotocom/ | grep -v /yotiprotoattr/)
     for d in $dirs
