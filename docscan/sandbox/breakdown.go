45f3920e1d5f9d9dc32c351d8f96c7b53dcb12cf
David Grayston <davidgrayston@googlemail.com>
2020-07-14 17:41:04 +0100

    SDK-1540: Implement document authenticity check

--- docscan/sandbox/breakdown.go
+++ docscan/sandbox/breakdown.go
@@ -1,35 +1,53 @@
 package sandbox
 
 // Breakdown describes a breakdown on check
-type Breakdown struct {
+type breakdown struct {
 	SubCheck string   `json:"sub_check"`
 	Result   string   `json:"result"`
-	Details  []Detail `json:"details"`
+	Details  []detail `json:"details"`
+}
+
+type breakdownBuilder struct {
+	subCheck string
+	result   string
+	details  []detail
 }
 
 // Detail is an individual breakdown detail
-type Detail struct {
+type detail struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
 
+func NewBreakdownBuilder() *breakdownBuilder {
+	return &breakdownBuilder{}
+}
+
 // WithSubCheck sets the sub check of a Breakdown
-func (breakdown Breakdown) WithSubCheck(subCheck string) Breakdown {
-	breakdown.SubCheck = subCheck
-	return breakdown
+func (b *breakdownBuilder) WithSubCheck(subCheck string) *breakdownBuilder {
+	b.subCheck = subCheck
+	return b
 }
 
 // WithResult sets the result of a Breakdown
-func (breakdown Breakdown) WithResult(result string) Breakdown {
-	breakdown.Result = result
-	return breakdown
+func (b *breakdownBuilder) WithResult(result string) *breakdownBuilder {
+	b.result = result
+	return b
 }
 
 // WithDetail sets the Detail of a Breakdown
-func (breakdown Breakdown) WithDetail(name string, value string) Breakdown {
-	breakdown.Details = append(breakdown.Details, Detail{
+func (b *breakdownBuilder) WithDetail(name string, value string) *breakdownBuilder {
+	b.details = append(b.details, detail{
 		Name:  name,
 		Value: value,
 	})
-	return breakdown
+	return b
+}
+
+func (b *breakdownBuilder) Build() breakdown {
+	return breakdown{
+		SubCheck: b.subCheck,
+		Result:   b.result,
+		Details:  b.details,
+	}
 }
