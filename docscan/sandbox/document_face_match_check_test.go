393ffcb564c9c108444870433f9230a6bcc9160f
David Grayston <davidgrayston@googlemail.com>
2020-07-14 21:30:17 +0100

    SDK-1540: Implement document face match check

---
+++ docscan/sandbox/document_face_match_check_test.go
@@ -0,0 +1,22 @@
+package sandbox
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func Example_documentFaceMatchCheckBuilder() {
+	breakdown, _ := NewBreakdownBuilder().Build()
+	recommendation, _ := NewRecommendationBuilder().Build()
+	filter, _ := NewDocumentFilterBuilder().Build()
+
+	check, _ := NewDocumentFaceMatchCheckBuilder().
+		WithBreakdown(breakdown).
+		WithRecommendation(recommendation).
+		WithDocumentFilter(filter).
+		Build()
+
+	data, _ := json.Marshal(check)
+	fmt.Println(string(data))
+	// Output: {"result":{"report":{"recommendation":{"value":"","reason":"","recovery_suggestion":""},"breakdown":[{"sub_check":"","result":"","details":null}]}},"document_filter":{"document_types":null,"country_codes":null}}
+}
