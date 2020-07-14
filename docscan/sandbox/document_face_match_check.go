f9c7b6e89645cc4870de655a5a0a2b80a3c28bba
David Grayston <davidgrayston@googlemail.com>
2020-07-14 22:08:49 +0100

    SDK-1540: Reduce duplication in builders

--- docscan/sandbox/document_face_match_check.go
+++ docscan/sandbox/document_face_match_check.go
@@ -1,12 +1,10 @@
 package sandbox
 
 type documentFaceMatchCheck struct {
-	check
 	documentCheck
 }
 
 type documentFaceMatchCheckBuilder struct {
-	checkBuilder
 	documentCheckBuilder
 	err error
 }
@@ -16,12 +14,12 @@ func NewDocumentFaceMatchCheckBuilder() *documentFaceMatchCheckBuilder {
 }
 
 func (b *documentFaceMatchCheckBuilder) WithRecommendation(recommendation recommendation) *documentFaceMatchCheckBuilder {
-	b.checkBuilder.withRecommendation(recommendation)
+	b.documentCheckBuilder.withRecommendation(recommendation)
 	return b
 }
 
 func (b *documentFaceMatchCheckBuilder) WithBreakdown(breakdown breakdown) *documentFaceMatchCheckBuilder {
-	b.checkBuilder.withBreakdown(breakdown)
+	b.documentCheckBuilder.withBreakdown(breakdown)
 	return b
 }
 
@@ -31,20 +29,14 @@ func (b *documentFaceMatchCheckBuilder) WithDocumentFilter(filter documentFilter
 }
 
 func (b *documentFaceMatchCheckBuilder) Build() (documentFaceMatchCheck, error) {
-	documentAuthenticityCheck := documentFaceMatchCheck{}
-
-	check, err := b.checkBuilder.build()
-	if err != nil {
-		return documentAuthenticityCheck, err
-	}
+	documentFaceMatchCheck := documentFaceMatchCheck{}
 
 	documentCheck, err := b.documentCheckBuilder.build()
 	if err != nil {
-		return documentAuthenticityCheck, err
+		return documentFaceMatchCheck, err
 	}
 
-	documentAuthenticityCheck.check = check
-	documentAuthenticityCheck.documentCheck = documentCheck
+	documentFaceMatchCheck.documentCheck = documentCheck
 
-	return documentAuthenticityCheck, b.err
+	return documentFaceMatchCheck, b.err
 }
