2ca74b1e1860cd976845b525b31d72ae59ebea49
David Grayston <davidgrayston@googlemail.com>
2020-07-15 16:26:15 +0100

    SDK-1540: Implement check reports

--- docscan/sandbox/check/document_face_match_check_test.go
+++ docscan/sandbox/check/document_face_match_check_test.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/getyoti/yoti-go-sdk/v3/docscan/sandbox"
 	"github.com/getyoti/yoti-go-sdk/v3/docscan/sandbox/check/report"
+	"github.com/getyoti/yoti-go-sdk/v3/docscan/sandbox/filter"
 )
 
 func Example_documentFaceMatchCheckBuilder() {
@@ -19,7 +19,7 @@ func Example_documentFaceMatchCheckBuilder() {
 		return
 	}
 
-	filter, err := sandbox.NewDocumentFilterBuilder().Build()
+	filter, err := filter.NewDocumentFilterBuilder().Build()
 	if err != nil {
 		return
 	}
