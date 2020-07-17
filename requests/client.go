5f9e204ba2a2bd6c95c85acb7259fb8bf9f771a1
echarrod <echarrod@users.noreply.github.com>
2020-07-17 14:15:25 +0100

    SDK-1659: Correct comment, remove .gitignore in /.idea (this is ignored in root .gitignore)

--- requests/client.go
+++ requests/client.go
@@ -15,7 +15,7 @@ var (
 	}
 )
 
-//HttpClient is a mockable HTTP Client Interface
+// HttpClient is a mockable HTTP Client Interface
 type HttpClient interface {
 	Do(*http.Request) (*http.Response, error)
 }
