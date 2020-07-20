a45fbc858113ae5b2c987b7c76dffbc08cdfd16e
echarrod <echarrod@users.noreply.github.com>
2020-07-20 13:15:58 +0100

    SDK-1661: Move anchor package to /profile/attribute

--- profile/attribute/generic_attribute.go
+++ profile/attribute/generic_attribute.go
@@ -3,7 +3,7 @@ package attribute
 import (
 	"log"
 
-	"github.com/getyoti/yoti-go-sdk/v3/anchor"
+	"github.com/getyoti/yoti-go-sdk/v3/profile/attribute/anchor"
 	"github.com/getyoti/yoti-go-sdk/v3/yotiprotoattr"
 )
 
