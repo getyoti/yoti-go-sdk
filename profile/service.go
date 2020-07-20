cebd5e7f96604c7ab07cb468db2228194e1cc4a6
David Grayston <davidgrayston@googlemail.com>
2020-07-20 19:54:10 +0100

    SDK-1659: Append body to error when available

--- profile/service.go
+++ profile/service.go
@@ -48,7 +48,7 @@ func GetActivityDetails(httpClient requests.HttpClient, token, clientSdkId, apiU
 		return
 	}
 
-	response, err := requests.Execute(httpClient, request, map[int]string{404: "%[1]d: Profile Not Found"}, requests.DefaultHTTPErrorMessages)
+	response, err := requests.Execute(httpClient, request, map[int]string{404: "Profile Not Found"}, requests.DefaultHTTPErrorMessages)
 	if err != nil {
 		return
 	}
