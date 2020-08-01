aa3b41b9282bd842f8a65d333c44a15ee94ad42a
echarrod <echarrod@users.noreply.github.com>
2020-08-01 09:29:05 +0100

    SDK-1661: Remove stutter from anchor types

--- profile/attribute/anchor/anchors_test.go
+++ profile/attribute/anchor/anchors_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestFilterAnchors_FilterSources(t *testing.T) {
 	anchorSlice := []*Anchor{
-		{subtype: "a", anchorType: AnchorTypeSource},
-		{subtype: "b", anchorType: AnchorTypeVerifier},
-		{subtype: "c", anchorType: AnchorTypeSource},
+		{subtype: "a", anchorType: TypeSource},
+		{subtype: "b", anchorType: TypeVerifier},
+		{subtype: "c", anchorType: TypeSource},
 	}
-	sources := filterAnchors(anchorSlice, AnchorTypeSource)
+	sources := filterAnchors(anchorSlice, TypeSource)
 	assert.Equal(t, len(sources), 2)
 	assert.Equal(t, sources[0].subtype, "a")
 	assert.Equal(t, sources[1].subtype, "c")
