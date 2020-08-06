8a7c95d531174344fd46bd96e1de8b27ab63c0a0
Ed Harrod <echarrod@users.noreply.github.com>
2020-08-06 13:02:42 +0100

    SDK-1300: Apply suggestions from code review
    Co-authored-by: David Grayston <1229335+davidgrayston@users.noreply.github.com>

--- media/mediavalue_test.go
+++ media/mediavalue_test.go
@@ -8,14 +8,14 @@ import (
 	"gotest.tools/v3/assert"
 )
 
-func TestMedia_Base64Selfie_Png(t *testing.T) {
+func TestMedia_Base64URL_Png(t *testing.T) {
 	imageBase64Value, media := createMedia("png")
 	expectedDataUrl := "data:image/png;base64," + imageBase64Value
 
 	assert.Equal(t, expectedDataUrl, media.Base64URL())
 }
 
-func TestMedia_Base64Selfie_Jpeg(t *testing.T) {
+func TestMedia_Base64URL_Jpeg(t *testing.T) {
 	imageBase64Value, media := createMedia("jpeg")
 	expectedDataUrl := "data:image/jpeg;base64," + imageBase64Value
 
