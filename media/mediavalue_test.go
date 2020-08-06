f887c4e750420e379e2fea0dc75eca2e688e2407
echarrod <echarrod@users.noreply.github.com>
2020-08-06 13:33:56 +0100

    SDK-1300: Avoid shadowing of imageBase64Value

--- media/mediavalue_test.go
+++ media/mediavalue_test.go
@@ -9,27 +9,27 @@ import (
 )
 
 func TestMedia_Base64URL_Png(t *testing.T) {
-	imageBase64Value, media := createMedia("png")
-	expectedDataUrl := "data:image/png;base64," + imageBase64Value
+	mediaBase64Value, media := createMedia("png")
+	expectedDataUrl := "data:image/png;base64," + mediaBase64Value
 
 	assert.Equal(t, expectedDataUrl, media.Base64URL())
 }
 
 func TestMedia_Base64URL_Jpeg(t *testing.T) {
-	imageBase64Value, media := createMedia("jpeg")
-	expectedDataUrl := "data:image/jpeg;base64," + imageBase64Value
+	mediaBase64Value, media := createMedia("jpeg")
+	expectedDataUrl := "data:image/jpeg;base64," + mediaBase64Value
 
 	assert.Equal(t, expectedDataUrl, media.Base64URL())
 }
 
 func createMedia(contentType string) (string, *Value) {
 	imageBytes := []byte("value")
-	imageBase64Value := base64.StdEncoding.EncodeToString(imageBytes)
+	mediaBase64Value := base64.StdEncoding.EncodeToString(imageBytes)
 	MIMEType := fmt.Sprintf("image/%s", contentType)
 
 	media := &Value{
 		MIMEType: MIMEType,
 		Data:     imageBytes,
 	}
-	return imageBase64Value, media
+	return mediaBase64Value, media
 }
