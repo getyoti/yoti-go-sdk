a55f9517d6b9f89520251ac77c15ca398c903fc2
Leonard Kramer <leonard.kramer@yoti.com>
2020-08-07 10:25:25 +0100

    SDK-1300: Create a Generic media type.

--- media/mediavalue.go
+++ media/mediavalue.go
@@ -10,14 +10,51 @@ type Media interface {
 	// Base64URL is the media encoded as a base64 URL.
 	Base64URL() string
 
-	// MIMETYPE returns the media's MIME type.
-	MIMEType() MIMEType
+	// MIME returns the media's MIME type.
+	MIME() string
 
 	// Data returns the media's raw data.
 	Data() []byte
 }
 
-func base64URL(mimeType MIMEType, data []byte) string {
+// NewMedia will create a new appropriate media structure based on the MIME
+// type provided. If no suitable structure exists, a Generic one will be used.
+func NewMedia(mime string, data []byte) Media {
+	switch mime {
+	case ImageTypeJPEG:
+		return JPEGImage(data)
+	case ImageTypePNG:
+		return PNGImage(data)
+	default:
+		return NewGeneric(mime, data)
+	}
+}
+
+type Generic struct {
+	mime string
+	data []byte
+}
+
+func NewGeneric(mime string, data []byte) Generic {
+	return Generic{
+		mime: mime,
+		data: data,
+	}
+}
+
+func (g Generic) MIME() string {
+	return g.mime
+}
+
+func (g Generic) Base64URL() string {
+	return base64URL(g.MIME(), g.data)
+}
+
+func (g Generic) Data() []byte {
+	return g.data
+}
+
+func base64URL(mimeType string, data []byte) string {
 	base64EncodedImage := base64.StdEncoding.EncodeToString(data)
 	return fmt.Sprintf("data:%s;base64,%s", mimeType, base64EncodedImage)
 }
