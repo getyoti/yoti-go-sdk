548ba5d525ca3a81771c3e58e86448eef640378a
echarrod <echarrod@users.noreply.github.com>
2020-08-07 11:30:37 +0100

    SDK-1300: Remove CreateImage: no longer needed after CreateImage removed

--- media/image_test.go
+++ media/image_test.go
@@ -10,15 +10,6 @@ const (
 	imageBase64Value = "dmFsdWU="
 )
 
-func TestImage_Base64URL_CreateImage(t *testing.T) {
-	imageBytes := []byte("value")
-
-	result := PNGImage(imageBytes)
-	expectedDataURL := "data:image/png;base64," + imageBase64Value
-
-	assert.Equal(t, expectedDataURL, result.Base64URL())
-}
-
 func TestImage_Base64URL_CreateJpegImage(t *testing.T) {
 	imageBytes := []byte("value")
 
