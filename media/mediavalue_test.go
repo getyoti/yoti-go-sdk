4c7bf7d95fc71c9002959939a8e8c88cdb64c010
Leonard Kramer <leonard.kramer@yoti.com>
2020-08-07 11:15:15 +0100

    SDK-1300: Add documentation to all exported functions.

--- media/mediavalue_test.go
+++ media/mediavalue_test.go
@@ -12,9 +12,9 @@ func TestGeneric_Base64URL_create(t *testing.T) {
 
 	result := NewGeneric(mime, databytes)
 
-	expectedDataUrl := "data:" + mime + ";base64," + imageBase64Value
+	expectedDataURL := "data:" + mime + ";base64," + imageBase64Value
 
-	assert.Equal(t, expectedDataUrl, result.Base64URL())
+	assert.Equal(t, expectedDataURL, result.Base64URL())
 }
 
 func TestNewMedia(t *testing.T) {
