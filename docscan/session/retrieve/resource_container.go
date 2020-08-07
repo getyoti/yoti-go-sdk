9b34f6c74ee8476fba0ec63976af3fdf15b21340
echarrod <echarrod@users.noreply.github.com>
2020-08-07 17:15:15 +0100

    SDK-1300: Add omitempty, zoom liveness filter

--- docscan/session/retrieve/resource_container.go
+++ docscan/session/retrieve/resource_container.go
@@ -1,9 +1,19 @@
 package retrieve
 
-// ResourceContainer TODO: add
+import "github.com/getyoti/yoti-go-sdk/v3/util"
+
+// ResourceContainer contains different resources that are part of the Yoti Doc Scan session
 type ResourceContainer struct {
 	IDDocuments     []IDDocumentResourceResponse `json:"id_documents"`
 	LivenessCapture []LivenessResourceResponse   `json:"liveness_capture"`
 }
 
-//TODO: add ZoomLivenessResources filter
+// GetZoomLivenessResources returns a filtered slice of Zoom liveness capture resources
+func (rc ResourceContainer) GetZoomLivenessResources() []ZoomLivenessResourceResponse {
+	filteredResources := util.Filter(rc.LivenessCapture, func(val interface{}) bool {
+		_, isZoomLivenessResource := val.(TextExtractionTaskResponse)
+		return isZoomLivenessResource
+	})
+
+	return filteredResources.([]ZoomLivenessResourceResponse)
+}
