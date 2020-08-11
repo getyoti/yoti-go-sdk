ef987b7f027a63c4d99cc21feaf696e42b16528f
echarrod <echarrod@users.noreply.github.com>
2020-08-11 11:04:37 +0100

    SDK-1300: Add tests for other RequestedChecks in same payload for SessionSpec

--- docscan/session/create/session_spec_test.go
+++ docscan/session/create/session_spec_test.go
@@ -21,7 +21,21 @@ func ExampleSessionSpecificationBuilder_Build() {
 	faceMatchCheck, err := check.NewRequestedFaceMatchCheckBuilder().
 		WithManualCheckNever().
 		Build()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error: %s", err.Error())
+		return
+	}
 
+	documentAuthenticityCheck, err := check.NewRequestedDocumentAuthenticityCheckBuilder().
+		Build()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error: %s", err.Error())
+		return
+	}
+
+	livenessCheck, err := check.NewRequestedLivenessCheckBuilder().
+		WithMaxRetries(5).
+		Build()
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "error: %s", err.Error())
 		return
@@ -33,6 +47,8 @@ func ExampleSessionSpecificationBuilder_Build() {
 		WithUserTrackingID("some-tracking-id").
 		WithNotifications(notifications).
 		WithRequestedCheck(faceMatchCheck).
+		WithRequestedCheck(documentAuthenticityCheck).
+		WithRequestedCheck(livenessCheck).
 		// WithRequestedTasks().
 		// WithSdkConfig().
 		Build()
@@ -44,5 +60,5 @@ func ExampleSessionSpecificationBuilder_Build() {
 
 	data, _ := json.Marshal(sessionSpecification)
 	fmt.Println(string(data))
-	// Output: {"client_session_token_ttl":789,"resources_ttl":456,"user_tracking_id":"some-tracking-id","notifications":{"topics":["some-topic"]},"requested_checks":[{"type":"ID_DOCUMENT_FACE_MATCH","config":{"manual_check":"NEVER"}}]}
+	// Output: {"client_session_token_ttl":789,"resources_ttl":456,"user_tracking_id":"some-tracking-id","notifications":{"topics":["some-topic"]},"requested_checks":[{"type":"ID_DOCUMENT_FACE_MATCH","config":{"manual_check":"NEVER"}},{"type":"ID_DOCUMENT_AUTHENTICITY","config":{}},{"type":"LIVENESS","config":{"max_retries":5,"liveness_type":""}}]}
 }
