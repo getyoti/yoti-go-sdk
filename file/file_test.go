4b22a553d06214c1923c89bae7a77397665219c4
David Grayston <davidgrayston@googlemail.com>
2020-08-11 16:03:34 +0100

    SDK-1690: Add test coverage for file

--- file/file_test.go
+++ file/file_test.go
@@ -6,6 +6,11 @@ import (
 	"gotest.tools/v3/assert"
 )
 
+func TestFile_ReadFile(t *testing.T) {
+	_, err := ReadFile("../test/test-key.pem")
+	assert.NilError(t, err)
+}
+
 func TestFile_ReadFile_ShouldFailForFileNotFound(t *testing.T) {
 	MissingFileName := "/tmp/file_not_found"
 	_, err := ReadFile(MissingFileName)
