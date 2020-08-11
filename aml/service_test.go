586b0b34b0673eb079bccfea3175a498a324999e
David Grayston <davidgrayston@googlemail.com>
2020-08-11 14:42:50 +0100

    SDK-1690: Add test coverage for AML service

--- aml/service_test.go
+++ aml/service_test.go
@@ -2,6 +2,7 @@ package aml
 
 import (
 	"crypto/rsa"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -21,6 +22,25 @@ func (mock *mockHTTPClient) Do(request *http.Request) (*http.Response, error) {
 		return mock.do(request)
 	}
 	return nil, nil
+}
+
+type mockReadCloser struct {
+	read  func(p []byte) (n int, err error)
+	close func() error
+}
+
+func (mock *mockReadCloser) Read(p []byte) (n int, err error) {
+	if mock.read != nil {
+		return mock.read(p)
+	}
+	return 0, nil
+}
+
+func (mock *mockReadCloser) Close() error {
+	if mock.close != nil {
+		return mock.close()
+	}
+	return nil
 }
 
 func TestPerformCheck_WithInvalidJSON(t *testing.T) {
@@ -79,6 +99,43 @@ func TestPerformCheck_Unsuccessful(t *testing.T) {
 		Temporary() bool
 	})
 	assert.Check(t, temporary && tempError.Temporary())
+}
+
+func TestPerformCheck_ShouldReturnMissingBaseURLError(t *testing.T) {
+	key := getValidKey()
+
+	client := &mockHTTPClient{
+		do: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+			}, nil
+		},
+	}
+
+	_, err := PerformCheck(client, createStandardProfile(), "clientSdkId", "", key)
+	assert.ErrorContains(t, err, "missing BaseURL")
+}
+
+func TestPerformCheck_ShouldReturnBodyError(t *testing.T) {
+	key := getValidKey()
+
+	body := &mockReadCloser{
+		read: func(p []byte) (n int, err error) {
+			return 0, errors.New("some read error")
+		},
+	}
+
+	client := &mockHTTPClient{
+		do: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       body,
+			}, nil
+		},
+	}
+
+	_, err := PerformCheck(client, createStandardProfile(), "clientSdkId", "https://apiUrl", key)
+	assert.ErrorContains(t, err, "some read error")
 }
 
 func getValidKey() *rsa.PrivateKey {
