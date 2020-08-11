c754d23da84e7db23cad259ff52564d522eb2611
echarrod <echarrod@users.noreply.github.com>
2020-08-11 18:11:44 +0100

    SDK-1300: Revert to using strings instead of aliased types - to alow for future items to be added without an update. Rename filter var names to avoid clash with package

--- docscan/session/create/filter/constants.go
+++ docscan/session/create/filter/constants.go
@@ -1,19 +1,12 @@
 package filter
 
-type documentType string
-
-const identity documentType = "ID_DOCUMENT"
-
-// type restrictions string
-
-// const (
-// 	orthogonal restrictions = "ORTHOGONAL_RESTRICTIONS"
-// 	document   restrictions = "DOCUMENT_RESTRICTIONS"
-// )
-
-type inclusionType string
-
 const (
-	includeList inclusionType = "WHITELIST"
-	excludeList inclusionType = "BLACKLIST"
+	includeList string = "WHITELIST"
+	excludeList string = "BLACKLIST"
+
+	identityDocument string = "ID_DOCUMENT"
+
+// 	orthogonalRestriction string = "ORTHOGONAL_RESTRICTIONS"
+// 	documentRestriction   string = "DOCUMENT_RESTRICTIONS"
+
 )
