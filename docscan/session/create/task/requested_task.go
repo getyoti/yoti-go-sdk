838189e7cab28104026f0ec2feaebae55f0b091e
echarrod <echarrod@users.noreply.github.com>
2020-08-12 11:10:44 +0100

    SDK-1300: Change RequestedTask to be interface

--- docscan/session/create/task/requested_task.go
+++ docscan/session/create/task/requested_task.go
@@ -1,11 +1,12 @@
 package task
 
 // RequestedTask requests creation of a Task to be performed on each document
-type RequestedTask struct {
-	Type   string              `json:"type"`
-	Config RequestedTaskConfig `json:"config"`
+type RequestedTask interface {
+	Type() string
+	Config() RequestedTaskConfig
+	MarshalJSON() ([]byte, error)
 }
 
 // RequestedTaskConfig  is the configuration applied when creating a Task
-type RequestedTaskConfig struct {
+type RequestedTaskConfig interface {
 }
