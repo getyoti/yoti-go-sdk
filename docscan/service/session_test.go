d7d4ee4158281060bc21dfdb95d4745397f85d2d
echarrod <echarrod@users.noreply.github.com>
2020-08-13 10:47:35 +0100

    SDK-1300: Move CreateSessionResult to create package, rename to (create.)SessionResult, add tests

---
+++ docscan/service/session_test.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/getyoti/yoti-go-sdk/v3/docscan/session/create"
+	"gotest.tools/v3/assert"
+)
+
+type mockHTTPClient struct {
+	do func(*http.Request) (*http.Response, error)
+}
+
+
+func (mock *mockHTTPClient) Do(request *http.Request) (*http.Response, error) {
+	if mock.do != nil {
+		return mock.do(request)
+	}
+	return nil, nil
+}
+
+func ExampleCreateSession() {
+	// session := CreateSession(nil, )
+}
+
+func TestCreateSession(t *testing.T) {
+	key, _ := rsa.GenerateKey(rand.Reader, 1024)
+
+	var clientSessionTokenTTL int = 100
+	var clientSessionToken string = "8c91671a-7194-4ad7-8483-32703b965cfc"
+	var sessionId string = "c87c4f2a-13fd-4cc8-a0e4-f1637cf32f71"
+
+	jsonResponse := fmt.Sprintf(`{"client_session_token_ttl":%d,"client_session_token":"%s","session_id":"%s"}`, clientSessionTokenTTL, clientSessionToken, sessionId)
+	// jsonResponse := fmt.Sprintf(, clientSessionTokenTTL, clientSessionToken, sessionId)
+
+	HTTPClient := &mockHTTPClient{
+		do: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 201,
+				Body:       ioutil.NopCloser(strings.NewReader(jsonResponse)),
+			}, nil
+		},
+	}
+
+	var sessionSpec *create.SessionSpecification
+	createSessionResult, err := CreateSession(HTTPClient, "sdkId", key, "https://apiurl.com", sessionSpec, nil)
+	assert.NilError(t, err)
+
+	assert.Equal(t, clientSessionTokenTTL, createSessionResult.ClientSessionTokenTTL)
+	assert.Equal(t, clientSessionToken, createSessionResult.ClientSessionToken)
+	assert.Equal(t, sessionId, createSessionResult.SessionID)
+}
