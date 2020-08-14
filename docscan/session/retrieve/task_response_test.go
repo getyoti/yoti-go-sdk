a030d81ff8335252c938efd5cae8e28c9eba9221
echarrod <echarrod@users.noreply.github.com>
2020-08-14 16:35:07 +0100

    SDK-1300: Add json subtype parsing with custom JSON unmarshalling

---
+++ docscan/session/retrieve/task_response_test.go
@@ -0,0 +1,33 @@
+package retrieve
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/getyoti/yoti-go-sdk/v3/docscan/constants"
+	"gotest.tools/v3/assert"
+)
+
+func TestTaskResponse_UnmarshalJSON(t *testing.T) {
+	generatedTextDataCheck := GeneratedCheckResponse{
+		Type: constants.IDDocumentTextDataCheck,
+		ID:   "some-id",
+	}
+
+	var checks []*GeneratedCheckResponse
+	checks = append(checks, &GeneratedCheckResponse{Type: "OTHER_TYPE", ID: "other-id"})
+	checks = append(checks, &generatedTextDataCheck)
+
+	taskResponse := TaskResponse{
+		GeneratedChecks: checks,
+	}
+	marshalled, err := json.Marshal(&taskResponse)
+	assert.NilError(t, err)
+
+	var result TaskResponse
+	err = json.Unmarshal(marshalled, &result)
+	assert.NilError(t, err)
+
+	assert.Equal(t, 1, len(result.GeneratedTextDataChecks()))
+	assert.Equal(t, "some-id", result.GeneratedTextDataChecks()[0].ID)
+}
--- docscan/session/retrieve/text_extraction_task_response.go
+++
@@ -1,6 +0,0 @@
-package retrieve
-
-// TextExtractionTaskResponse represents a Text Extraction task response
-type TextExtractionTaskResponse struct {
-	TaskResponse
-}
