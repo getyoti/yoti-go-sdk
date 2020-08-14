8b391c573002b160d153f8b01ff54103aea6add7
echarrod <echarrod@users.noreply.github.com>
2020-08-14 18:07:26 +0100

    SDK-1300: Remove TODO (done)

--- docscan/client.go
+++ docscan/client.go
@@ -153,7 +153,7 @@ func (c *Client) DeleteSession(sessionID string) error {
 }
 
 // GetMediaContent retrieves media related to a Yoti Doc Scan session based on the supplied media ID
-func (c *Client) GetMediaContent(sessionID, mediaID string) (*media.Media, error) { // TODO: change to media.Value
+func (c *Client) GetMediaContent(sessionID, mediaID string) (*media.Media, error) {
 	request, err := (&requests.SignedRequest{
 		Key:        c.Key,
 		HTTPMethod: http.MethodGet,
