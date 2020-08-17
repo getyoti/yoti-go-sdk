d2d047051c44f91f58f97d0ba7849aa71c002394
echarrod <echarrod@users.noreply.github.com>
2020-08-17 17:49:38 +0100

    SDK-1300: Move Zoom constant, use pointers

--- docscan/session/retrieve/zoom_liveness_resource_response.go
+++ docscan/session/retrieve/zoom_liveness_resource_response.go
@@ -2,7 +2,7 @@ package retrieve
 
 // ZoomLivenessResourceResponse represents a Zoom Liveness resource for a given session
 type ZoomLivenessResourceResponse struct {
-	LivenessResourceResponse
-	FaceMap FaceMapResponse `json:"facemap"`
-	Frames  []FrameResponse `json:"frames"`
+	*LivenessResourceResponse
+	FaceMap *FaceMapResponse `json:"facemap"`
+	Frames  []*FrameResponse `json:"frames"`
 }
