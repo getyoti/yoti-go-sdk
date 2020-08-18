6917ac1139e8db110b7f814ed9ae26f56e43cafe
echarrod <echarrod@users.noreply.github.com>
2020-08-18 10:41:47 +0100

    SDK-1300: Don't use pointer for time.Time
    From godocs: "Programs using times should typically store and pass them as values, not pointers. That is, time variables and struct fields should be of type time.Time, not *time.Time." https://golang.org/src/time/time.go

--- docscan/session/retrieve/task_response.go
+++ docscan/session/retrieve/task_response.go
@@ -12,8 +12,8 @@ type TaskResponse struct {
 	ID                      string                    `json:"id"`
 	Type                    string                    `json:"type"`
 	State                   string                    `json:"state"`
-	Created                 *time.Time                `json:"created"`
-	LastUpdated             *time.Time                `json:"last_updated"`
+	Created                 time.Time                 `json:"created"`
+	LastUpdated             time.Time                 `json:"last_updated"`
 	GeneratedChecks         []*GeneratedCheckResponse `json:"generated_checks"`
 	GeneratedMedia          []*GeneratedMedia         `json:"generated_media"`
 	generatedTextDataChecks []*GeneratedTextDataCheckResponse
