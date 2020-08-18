ad81fc1768be5077043ff03505d6c315584b433a
echarrod <echarrod@users.noreply.github.com>
2020-08-18 08:48:53 +0100

    SDK-1300: Add remaining JSON subtype parsing

--- docscan/session/retrieve/resource_response.go
+++ docscan/session/retrieve/resource_response.go
@@ -2,6 +2,6 @@ package retrieve
 
 // ResourceResponse represents a resource, with associated tasks
 type ResourceResponse struct {
-	ID    string         `json:"id"`
-	Tasks []TaskResponse `json:"tasks"`
+	ID    string          `json:"id"`
+	Tasks []*TaskResponse `json:"tasks"`
 }
