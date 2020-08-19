29b8d98971692af7e9edd85c0acf1d94beaaba34
echarrod <echarrod@users.noreply.github.com>
2020-08-19 16:07:33 +0100

    SDK-1300: Use pointers for time.Time values which might be nil.
    I know we should _typically_ store and pass time.Time as values, not pointers, but we don't want to be misleading if these values aren't present. It's clearer to return nil, rather than return the zero value, which might be missed by clients

--- docscan/session/retrieve/media_response.go
+++ docscan/session/retrieve/media_response.go
@@ -4,8 +4,8 @@ import "time"
 
 // MediaResponse represents a media resource
 type MediaResponse struct {
-	ID          string    `json:"id"`
-	Type        string    `json:"type"`
-	Created     time.Time `json:"created"`
-	LastUpdated time.Time `json:"last_updated"`
+	ID          string     `json:"id"`
+	Type        string     `json:"type"`
+	Created     *time.Time `json:"created"`
+	LastUpdated *time.Time `json:"last_updated"`
 }
