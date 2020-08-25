6b80116004c3c7f8e8bfc81eec3a9355bf209576
echarrod <echarrod@users.noreply.github.com>
2020-08-25 14:32:08 +0100

    NA: Remove ClientInterface - not needed any more

--- requests/client.go
+++ requests/client.go
@@ -8,10 +8,3 @@ import (
 type HttpClient interface {
 	Do(*http.Request) (*http.Response, error)
 }
-
-// ClientInterface defines the interface required to Mock the YotiClient for
-// testing
-type ClientInterface interface {
-	MakeRequest(string, string, []byte, bool, ...map[int]string) (string, error)
-	GetSdkID() string
-}
