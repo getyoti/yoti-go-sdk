c068454d2e8d3cafaf7914df2e4b11c29e68eb57
echarrod <echarrod@users.noreply.github.com>
2020-08-26 15:20:41 +0100

    SDK-1300: Add docs

--- docscan/supported/supported_documents.go
+++ docscan/supported/supported_documents.go
@@ -1,14 +1,17 @@
 package supported
 
+// DocumentsResponse details the supported countries and associated documents
 type DocumentsResponse struct {
 	SupportedCountries []*Country `json:"supported_countries"`
 }
 
+// Country details the supported documents for a particular country
 type Country struct {
 	Code               string      `json:"code"`
 	SupportedDocuments []*Document `json:"supported_documents"`
 }
 
+// Document is the document type that is supported
 type Document struct {
 	Type string `json:"type"`
 }
