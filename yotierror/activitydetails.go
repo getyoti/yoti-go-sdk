b64f31efee13ecf8e60a799f5773e1ae187dbe5d
echarrod <echarrod@users.noreply.github.com>
2020-08-27 16:48:32 +0100

    SDK-1728: Define errors in yotierrros.activitydetails, for those that do not contain dynamic information

---
+++ yotierror/activitydetails.go
@@ -0,0 +1,14 @@
+package yotierror
+
+import "errors"
+
+var (
+	// InvalidTokenError means that the token used to call GetActivityDetails is invalid. Make sure you are retrieving this token correctly.
+	InvalidTokenError = errors.New("invalid Token")
+
+	// TokenDecryptError means that the token could not be decrypted. Ensure you are using the correct .pem file.
+	TokenDecryptError = errors.New("unable to decrypt token")
+
+	// SharingFailureError means that the share between a user and an application was not successful.
+	SharingFailureError = errors.New("sharing failure")
+)
