fc1c270cf1a5a0beaba4635b83672fd3d219d33f
echarrod <echarrod@users.noreply.github.com>
2020-08-27 17:32:29 +0100

    SDK-1728: Fix error casing

--- yotierror/activitydetails.go
+++ yotierror/activitydetails.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	// InvalidTokenError means that the token used to call GetActivityDetails is invalid. Make sure you are retrieving this token correctly.
-	InvalidTokenError = errors.New("invalid Token")
+	InvalidTokenError = errors.New("invalid token")
 
 	// TokenDecryptError means that the token could not be decrypted. Ensure you are using the correct .pem file.
 	TokenDecryptError = errors.New("unable to decrypt token")
