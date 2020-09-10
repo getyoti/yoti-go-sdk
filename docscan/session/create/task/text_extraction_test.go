e801f2faa544f51f92763c942c8b7672888d8a45
echarrod <echarrod@users.noreply.github.com>
2020-09-10 11:28:06 +0100

    SDk-1300: Add further test coverage

--- docscan/session/create/task/text_extraction_test.go
+++ docscan/session/create/task/text_extraction_test.go
@@ -3,6 +3,9 @@ package task
 import (
 	"encoding/json"
 	"fmt"
+	"testing"
+
+	"gotest.tools/v3/assert"
 )
 
 func ExampleRequestedTextExtractionTaskBuilder() {
@@ -18,4 +21,18 @@ func ExampleRequestedTextExtractionTaskBuilder() {
 	data, _ := json.Marshal(task)
 	fmt.Println(string(data))
 	// Output: {"type":"ID_DOCUMENT_TEXT_DATA_EXTRACTION","config":{"manual_check":"ALWAYS","chip_data":"IGNORE"}}
+}
+
+func TestRequestedTextExtractionTaskBuilder_Build_UsesLastManualCheck(t *testing.T) {
+	task, err := NewRequestedTextExtractionTaskBuilder().
+		WithManualCheckAlways().
+		WithManualCheckNever().
+		WithManualCheckFallback().
+		Build()
+	if err != nil {
+		t.Fail()
+	}
+
+	config := task.Config().(RequestedTextExtractionTaskConfig)
+	assert.Equal(t, "FALLBACK", config.ManualCheck)
 }
--- docscan/session/retrieve/resource_container_test.go
+++ docscan/session/retrieve/resource_container_test.go
@@ -15,7 +15,10 @@ func TestLivenessResourceResponse_UnmarshalJSON(t *testing.T) {
 	var result ResourceContainer
 	err = json.Unmarshal(bytes, &result)
 	assert.NilError(t, err)
+
+	assert.Equal(t, 2, len(result.LivenessCapture))
 	assert.Equal(t, "ZOOM", result.LivenessCapture[0].LivenessType)
+	assert.Equal(t, "OTHER_LIVENESS_TYPE", result.LivenessCapture[1].LivenessType)
 
 	assert.Equal(t, "IMAGE", result.ZoomLivenessResources()[0].Frames[0].Media.Type)
 	assert.Equal(t, "BINARY", result.ZoomLivenessResources()[0].FaceMap.Media.Type)
