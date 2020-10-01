eb7ba77cc42f22a901ad2c658afd610b37bbb618
echarrod <echarrod@users.noreply.github.com>
2020-10-01 17:02:48 +0100

    SDK-1733: Add ability to configure manual fallback value on Document Authenticity Check

--- docscan/session/create/check/document_authenticity_test.go
+++ docscan/session/create/check/document_authenticity_test.go
@@ -3,17 +3,77 @@ package check
 import (
 	"encoding/json"
 	"fmt"
+	"testing"
+
+	"gotest.tools/v3/assert"
 )
 
 func ExampleRequestedDocumentAuthenticityCheckBuilder() {
-	check, err := NewRequestedDocumentAuthenticityCheckBuilder().Build()
+	docAuthCheck, err := NewRequestedDocumentAuthenticityCheckBuilder().WithManualCheckFallback().Build()
 
 	if err != nil {
 		fmt.Printf("error: %s", err.Error())
 		return
 	}
 
-	data, _ := json.Marshal(check)
+	data, _ := json.Marshal(docAuthCheck)
 	fmt.Println(string(data))
-	// Output: {"type":"ID_DOCUMENT_AUTHENTICITY","config":{}}
+	// Output: {"type":"ID_DOCUMENT_AUTHENTICITY","config":{"manual_check":"FALLBACK"}}
+}
+
+func TestRequestedDocumentAuthenticityCheckBuilder_WithManualCheckAlways(t *testing.T) {
+	docAuthCheck, err := NewRequestedDocumentAuthenticityCheckBuilder().
+		WithManualCheckAlways().
+		Build()
+
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+
+	result := docAuthCheck.Config().(RequestedDocumentAuthenticityConfig)
+	assert.Equal(t, "ALWAYS", result.ManualCheck)
+}
+
+func TestRequestedDocumentAuthenticityCheckBuilder_WithManualCheckFallback(t *testing.T) {
+	docAuthCheck, err := NewRequestedDocumentAuthenticityCheckBuilder().
+		WithManualCheckFallback().
+		Build()
+
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+
+	result := docAuthCheck.Config().(RequestedDocumentAuthenticityConfig)
+	assert.Equal(t, "FALLBACK", result.ManualCheck)
+}
+
+func TestRequestedDocumentAuthenticityCheckBuilder_WithManualCheckNever(t *testing.T) {
+	docAuthCheck, err := NewRequestedDocumentAuthenticityCheckBuilder().
+		WithManualCheckNever().
+		Build()
+
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+
+	result := docAuthCheck.Config().(RequestedDocumentAuthenticityConfig)
+	assert.Equal(t, "NEVER", result.ManualCheck)
+}
+
+func TestRequestedDocumentAuthenticityCheckBuilder_UsesLastValue(t *testing.T) {
+	docAuthCheck, err := NewRequestedDocumentAuthenticityCheckBuilder().
+		WithManualCheckFallback().
+		WithManualCheckNever().
+		WithManualCheckAlways().
+		Build()
+
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+	result := docAuthCheck.Config().(RequestedDocumentAuthenticityConfig)
+	assert.Equal(t, "ALWAYS", result.ManualCheck)
 }
