ea8214e70024a6e3dbc28f4c79df7ba9007c2624
echarrod <echarrod@users.noreply.github.com>
2020-10-01 17:02:48 +0100

    SDK-1733: Add ability to configure manual fallback value on Document Authenticity Check

--- docscan/session/create/check/document_authenticity.go
+++ docscan/session/create/check/document_authenticity.go
@@ -36,6 +36,7 @@ func (c *RequestedDocumentAuthenticityCheck) MarshalJSON() ([]byte, error) {
 
 // RequestedDocumentAuthenticityConfig is the configuration applied when creating a Document Authenticity Check
 type RequestedDocumentAuthenticityConfig struct {
+	ManualCheck string `json:"manual_check,omitempty"`
 }
 
 // RequestedDocumentAuthenticityCheckBuilder builds a RequestedDocumentAuthenticityCheck
@@ -46,6 +47,24 @@ type RequestedDocumentAuthenticityCheckBuilder struct {
 // NewRequestedDocumentAuthenticityCheckBuilder creates a new DocumentAuthenticityCheckBuilder
 func NewRequestedDocumentAuthenticityCheckBuilder() *RequestedDocumentAuthenticityCheckBuilder {
 	return &RequestedDocumentAuthenticityCheckBuilder{}
+}
+
+// WithManualCheckAlways requires that a manual follow-up check is always performed
+func (b *RequestedDocumentAuthenticityCheckBuilder) WithManualCheckAlways() *RequestedDocumentAuthenticityCheckBuilder {
+	b.config.ManualCheck = constants.Always
+	return b
+}
+
+// WithManualCheckFallback requires that a manual follow-up check is performed only on failed Checks, and those with a low level of confidence
+func (b *RequestedDocumentAuthenticityCheckBuilder) WithManualCheckFallback() *RequestedDocumentAuthenticityCheckBuilder {
+	b.config.ManualCheck = constants.Fallback
+	return b
+}
+
+// WithManualCheckNever requires that only an automated Check is performed.  No manual follow-up Check will ever be initiated
+func (b *RequestedDocumentAuthenticityCheckBuilder) WithManualCheckNever() *RequestedDocumentAuthenticityCheckBuilder {
+	b.config.ManualCheck = constants.Never
+	return b
 }
 
 // Build builds the RequestedDocumentAuthenticityCheck
