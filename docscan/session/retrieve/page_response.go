0e4a22214c2d8a1d22c720709094d51eec35c863
David Grayston <davidgrayston@googlemail.com>
2020-10-09 12:56:42 +0100

    SDK-1773: Support document page frames

--- docscan/session/retrieve/page_response.go
+++ docscan/session/retrieve/page_response.go
@@ -2,6 +2,7 @@ package retrieve
 
 // PageResponse represents information about an uploaded document Page
 type PageResponse struct {
-	CaptureMethod string         `json:"capture_method"`
-	Media         *MediaResponse `json:"media"`
+	CaptureMethod string           `json:"capture_method"`
+	Media         *MediaResponse   `json:"media"`
+	Frames        []*FrameResponse `json:"frames"`
 }
