5e498a3f8ba56b583c0b822f76d97a3ceddf8d2b
Emma Smith <Emma.Smith@Yoti.com>
2021-02-18 11:16:44 +0000

    SDK-1833: Add debug statement to get create session payload

--- docscan/client.go
+++ docscan/client.go
@@ -70,6 +70,7 @@ func getAPIURL() string {
 // CreateSession creates a Doc Scan session using the supplied session specification
 func (c *Client) CreateSession(sessionSpec *create.SessionSpecification) (*create.SessionResult, error) {
 	requestBody, err := marshalJSON(c.jsonMarshaler, sessionSpec)
+	fmt.Printf("DEBUG: %s\n", requestBody)
 	if err != nil {
 		return nil, err
 	}
