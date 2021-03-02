e9c449af95c6594fd3cfb96fcf267041f9718921
Emma Smith <Emma.Smith@Yoti.com>
2021-03-02 16:00:37 +0000

    SDK-1833: Add manual check config to text extraction

--- docscan/sandbox/request/task/document_text_data_extraction_task.go
+++ docscan/sandbox/request/task/document_text_data_extraction_task.go
@@ -3,6 +3,7 @@ package task
 import (
 	"encoding/base64"
 
+	"github.com/getyoti/yoti-go-sdk/v3/docscan/constants"
 	"github.com/getyoti/yoti-go-sdk/v3/docscan/sandbox/request/filter"
 )
 
@@ -10,6 +11,11 @@ import (
 type DocumentTextDataExtractionTask struct {
 	*documentTask
 	Result documentTextDataExtractionTaskResult `json:"result"`
+	Config documentTextDataExtractionConfig     `json:"config,omitempty"`
+}
+
+type documentTextDataExtractionConfig struct {
+	ManualCheck string `json:"manual_check,omitempty"`
 }
 
 // DocumentTextDataExtractionTaskBuilder builds a DocumentTextDataExtractionTask
@@ -19,6 +25,7 @@ type DocumentTextDataExtractionTaskBuilder struct {
 	documentIDPhoto *documentIDPhoto
 	detectedCountry string
 	recommendation  *TextDataExtractionRecommendation
+	manualCheck     string
 }
 
 type documentTextDataExtractionTaskResult struct {
@@ -80,6 +87,21 @@ func (b *DocumentTextDataExtractionTaskBuilder) WithRecommendation(recommendatio
 	return b
 }
 
+// WithManualCheckNever sets the manual check config to never
+func (b *DocumentTextDataExtractionTaskBuilder) WithManualCheckNever() {
+	b.manualCheck = constants.Never
+}
+
+// WithManualCheckAlways sets the manual check config to always
+func (b *DocumentTextDataExtractionTaskBuilder) WithManualCheckAlways() {
+	b.manualCheck = constants.Always
+}
+
+// WithManualCheckFallback sets the manual check config to fallback
+func (b *DocumentTextDataExtractionTaskBuilder) WithManualCheckFallback() {
+	b.manualCheck = constants.Fallback
+}
+
 // Build creates a new DocumentTextDataExtractionTask
 func (b *DocumentTextDataExtractionTaskBuilder) Build() (*DocumentTextDataExtractionTask, error) {
 	return &DocumentTextDataExtractionTask{
@@ -89,6 +111,9 @@ func (b *DocumentTextDataExtractionTaskBuilder) Build() (*DocumentTextDataExtrac
 			DocumentIDPhoto: b.documentIDPhoto,
 			DetectedCountry: b.detectedCountry,
 			Recommendation:  b.recommendation,
+		},
+		Config: documentTextDataExtractionConfig{
+			ManualCheck: b.manualCheck,
 		},
 	}, nil
 }
