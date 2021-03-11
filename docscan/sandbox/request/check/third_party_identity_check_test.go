2708a4246c02e38eeecec9dbc91aa23ae95aa585
Emma Smith <Emma.Smith@Yoti.com>
2021-03-11 15:51:16 +0000

    SDK-1833: Add coverage for ThirdPartyIdentityCheckBuilder

---
+++ docscan/sandbox/request/check/third_party_identity_check_test.go
@@ -0,0 +1,45 @@
+package check
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/getyoti/yoti-go-sdk/v3/docscan/sandbox/request/check/report"
+)
+
+func ExampleThirdPartyIdentityCheckBuilder() {
+	breakdown, err := report.NewBreakdownBuilder().
+		WithResult("some_result").
+		WithSubCheck("some_check").
+		Build()
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+
+	recommendation, err := report.NewRecommendationBuilder().
+		WithValue("some_value").
+		Build()
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+
+	check, err := NewThirdPartyIdentityCheckBuilder().
+		WithBreakdown(breakdown).
+		WithRecommendation(recommendation).
+		Build()
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+
+	data, err := json.Marshal(check)
+	if err != nil {
+		fmt.Printf("error: %s", err.Error())
+		return
+	}
+
+	fmt.Println(string(data))
+	// Output: {"result":{"report":{"recommendation":{"value":"some_value"},"breakdown":[{"sub_check":"some_check","result":"some_result","details":[]}]}}}
+}
