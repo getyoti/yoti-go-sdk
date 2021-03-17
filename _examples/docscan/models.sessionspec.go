c7b02b2bd1b760f8cec20af326894ae78a2c9422
Emma Smith <Emma.Smith@Yoti.com>
2021-03-17 14:24:41 +0000

    SDK-1833: Add Third Party Identity Check to docscan example project

--- _examples/docscan/models.sessionspec.go
+++ _examples/docscan/models.sessionspec.go
@@ -56,6 +56,13 @@ func buildSessionSpec() (sessionSpec *create.SessionSpecification, err error) {
 		return nil, err
 	}
 
+	var thirdPartyCheck *check.RequestedThirdPartyIdentityCheck
+	thirdPartyCheck, err = check.NewRequestedThirdPartyIdentityCheckBuilder().
+		Build()
+	if err != nil {
+		return nil, err
+	}
+
 	var sdkConfig *create.SDKConfig
 	sdkConfig, err = create.NewSdkConfigBuilder().
 		WithAllowsCameraAndUpload().
@@ -109,6 +116,7 @@ func buildSessionSpec() (sessionSpec *create.SessionSpecification, err error) {
 		WithRequestedCheck(documentAuthenticityCheck).
 		WithRequestedCheck(livenessCheck).
 		WithRequestedCheck(idDocsComparisonCheck).
+		WithRequestedCheck(thirdPartyCheck).
 		WithRequestedTask(textExtractionTask).
 		WithRequestedTask(supplementaryDocTextExtractionTask).
 		WithSDKConfig(sdkConfig).
