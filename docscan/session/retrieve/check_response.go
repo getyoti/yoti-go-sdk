6149566b125101fd811fe15d5f112cf647cb9e6e
Ed Harrod <echarrod@users.noreply.github.com>
2022-05-17 17:04:25 +0100

    Simplify structure of objects: we can't use JSON subtypes in the same way in Go: custom unmarshalling won't work unless we use Go 1.18, Generics, and polymorphic return types

--- docscan/session/retrieve/check_response.go
+++ docscan/session/retrieve/check_response.go
@@ -6,14 +6,15 @@ import (
 
 // CheckResponse represents the attributes of a check, for any given session
 type CheckResponse struct {
-	ID             string            `json:"id"`
-	Type           string            `json:"type"`
-	State          string            `json:"state"`
-	ResourcesUsed  []string          `json:"resources_used"`
-	GeneratedMedia []*GeneratedMedia `json:"generated_media"`
-	Report         *ReportResponse   `json:"report"`
-	Created        *time.Time        `json:"created"`
-	LastUpdated    *time.Time        `json:"last_updated"`
+	ID               string                   `json:"id"`
+	Type             string                   `json:"type"`
+	State            string                   `json:"state"`
+	ResourcesUsed    []string                 `json:"resources_used"`
+	GeneratedMedia   []*GeneratedMedia        `json:"generated_media"`
+	GeneratedProfile GeneratedProfileResponse `json:"generated_profile"`
+	Report           *ReportResponse          `json:"report"`
+	Created          *time.Time               `json:"created"`
+	LastUpdated      *time.Time               `json:"last_updated"`
 }
 
 // AuthenticityCheckResponse represents a Document Authenticity check for a given session
@@ -46,16 +47,12 @@ type SupplementaryDocumentTextDataCheckResponse struct {
 	*CheckResponse
 }
 
-type ProfileCheckResponse struct {
-	*CheckResponse
-}
-
 // ThirdPartyIdentityCheckResponse represents a check with an external credit reference agency
 type ThirdPartyIdentityCheckResponse struct {
-	*ProfileCheckResponse
+	*CheckResponse
 }
 
 // WatchlistScreeningCheckResponse represents a watchlist screening check
 type WatchlistScreeningCheckResponse struct {
-	*ProfileCheckResponse
+	*CheckResponse
 }
--- docscan/session/retrieve/watchlist.go
+++ docscan/session/retrieve/watchlist.go
@@ -4,7 +4,7 @@ type RawResults struct {
 	Media MediaResponse `json:"media"`
 }
 
-type WatchlistSummary struct {
+type WatchlistSummaryResponse struct {
 	TotalHits              int          `json:"total_hits"`
 	RawResults             RawResults   `json:"raw_results"`
 	AssociatedCountryCodes []string     `json:"associated_country_codes"`
