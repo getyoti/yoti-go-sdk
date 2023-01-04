4a5c1e2925bfe765f5e2e887bb349e72166cf174
mehmet-yoti <mehmet.sepici@yoti.com>
2023-01-04 14:14:32 +0000

    SDK-2155-go-static-liveness-check

--- docscan/session/retrieve/resource_container.go
+++ docscan/session/retrieve/resource_container.go
@@ -2,22 +2,31 @@ package retrieve
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/getyoti/yoti-go-sdk/v3/docscan/constants"
 )
 
 // ResourceContainer contains different resources that are part of the Yoti IDV session
 type ResourceContainer struct {
-	IDDocuments            []*IDDocumentResourceResponse            `json:"id_documents"`
-	SupplementaryDocuments []*SupplementaryDocumentResourceResponse `json:"supplementary_documents"`
-	LivenessCapture        []*LivenessResourceResponse
-	RawLivenessCapture     []json.RawMessage `json:"liveness_capture"`
-	zoomLivenessResources  []*ZoomLivenessResourceResponse
+	IDDocuments             []*IDDocumentResourceResponse            `json:"id_documents"`
+	SupplementaryDocuments  []*SupplementaryDocumentResourceResponse `json:"supplementary_documents"`
+	LivenessCapture         []*LivenessResourceResponse
+	RawLivenessCapture      []json.RawMessage `json:"liveness_capture"`
+	zoomLivenessResources   []*ZoomLivenessResourceResponse
+	staticLivenessResources []*StaticLivenessResourceResponse
 }
 
 // ZoomLivenessResources  filters the liveness resources, returning only the "Zoom" liveness resources
 func (r *ResourceContainer) ZoomLivenessResources() []*ZoomLivenessResourceResponse {
 	return r.zoomLivenessResources
+}
+
+// ZoomLivenessResources  filters the liveness resources, returning only the "Zoom" liveness resources
+func (r *ResourceContainer) StaticLivenessResources() []*StaticLivenessResourceResponse {
+	datas, _ := json.Marshal(r.staticLivenessResources)
+	fmt.Println(string(datas))
+	return r.staticLivenessResources
 }
 
 // UnmarshalJSON handles the custom JSON unmarshalling
@@ -43,6 +52,13 @@ func (r *ResourceContainer) UnmarshalJSON(data []byte) error {
 				return err
 			}
 			r.zoomLivenessResources = append(r.zoomLivenessResources, &zoom)
+		case constants.Static:
+			var static StaticLivenessResourceResponse
+			err = json.Unmarshal(raw, &static)
+			if err != nil {
+				return err
+			}
+			r.staticLivenessResources = append(r.staticLivenessResources, &static)
 		default:
 			err = json.Unmarshal(raw, &LivenessResourceResponse{})
 			if err != nil {
