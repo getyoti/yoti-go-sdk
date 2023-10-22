5912800eeb54744345e17c294042d86cb188ce87
mehmet-yoti <mehmet.sepici@yoti.com>
2023-10-22 23:53:36 +0100

    SDK-2259:added retrieve qr code

---
+++ digitalidentity/share_session_created.go
@@ -0,0 +1,8 @@
+package digitalidentity
+
+// ShareSessionCreated Share Session QR Result
+type ShareSessionCreated struct {
+	ID     string `json:"id"`
+	Satus  string `json:"status"`
+	Expiry string `json:"expiry"`
+}
