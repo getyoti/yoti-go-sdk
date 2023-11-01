22bbe7d45f848f597142880c8202799b59de2ad5
mehmet-yoti <mehmet.sepici@yoti.com>
2023-11-01 11:12:57 +0000

    SDK-2259:updated needed pr reviews

--- digitalidentity/share_session_created.go
+++
@@ -1,8 +0,0 @@
-package digitalidentity
-
-// ShareSessionCreated Share Session QR Result
-type ShareSessionCreated struct {
-	ID     string `json:"id"`
-	Satus  string `json:"status"`
-	Expiry string `json:"expiry"`
-}
